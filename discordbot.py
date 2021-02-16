b1fa619362c9a7cfa4b7d2bf13615c9d08fba179
Launcha10 <54789026+Launcha10@users.noreply.github.com>
2021-02-16 16:25:08 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -16,6 +16,10 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+    
+@bot.command()
+async def hello(ctx):
+    await ctx.send('bye')
 
 
 bot.run(token)
